68f6aca9b2c623dabbfa806298dc4c18d6943c5d
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2026-02-01 02:47:05 -0600

    fixed getRandomAttackValue- It was not returning a value. it now returns randomVal

--- game.js
+++ game.js
@@ -1,39 +1,42 @@
 function initializeGame() {
-  let strength = prompt("Enter your starting strength (e.g. 100):");
-  
+    let strength = prompt("Enter your starting strength (e.g. 100):");
 
-  if (strength == null ) {
-    document.getElementById("gameLog").innerText = "Game Canceled!"; 
-    return;
-  }
+
+    if (strength == null) {
+        document.getElementById("gameLog").innerText = "Game Canceled!";
+        return;
+    }
 
     startGameLoop(strength);
 }
 
 function startGameLoop(strength) {
-  let attacks = 0; 
 
-  while (strength > 0) {
-    let attackValue = getRandomAttackValue(strength);
-    strength -= attackValue;
-    attacks++;
-    if (strength < 0) {
-      strength = 0; 
+    let attacks = 0;
+
+    while (strength > 0) {
+        let attackValue = getRandomAttackValue(strength);
+
+        strength -= attackValue;
+        attacks++;
+        if (strength < 0) {
+            strength = 0;
+        }
+
+        logAttack(attacks, attackValue, strength);
     }
 
-    logAttack(attacks, attackValue, strength);
-  }
-
-  document.getElementById("result").innerText = "Game Over! Total number of attacks: " + attacks;
+    document.getElementById("result").innerText = "Game Over! Total number of attacks: " + attacks;
 }
 
 function getRandomAttackValue(max) {
 
     let randomVal = Math.floor(Math.random() * max) + 1;
+    return randomVal;
 }
 
 function logAttack(attacks, attackValue, strength) {
- document.getElementById("gameLog").innerHTML += "Attack " + attacks + ": " + attackValue + ". Remaining strength: " + strength + ".<br>";
+    document.getElementById("gameLog").innerHTML += "Attack " + attacks + ": " + attackValue + ". Remaining strength: " + strength + ".<br>";
 }
 
 // Start the game on load
